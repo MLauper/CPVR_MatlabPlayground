7e19380247e2f14ce50510c29dd4035e3dacf10f
MLauper <mauskatzehund@gmail.com>
2016-06-03 14:59:00 +0200

    Solved HoughLine_Shuttle

--- ex715_HoughLine_Shuttle.m
+++ ex715_HoughLine_Shuttle.m
@@ -9,7 +9,7 @@
 %##########################################################################
 clear all; close all; clc; %clear matrices, close figures & clear cmd wnd.
 
-RGB = imread('../images/Shuttle2.jpg');
+RGB = imread('./Shuttle2.jpg');
 I  = rgb2gray(RGB);     % convert to intensity
 BW = edge(I,'canny');   % extract edges
 imshow(BW);
@@ -28,5 +28,30 @@ P = houghpeaks(H,5);
 x = T(P(:,2)); y = R(P(:,1));  % Get the x & y coords out of T & P
 plot(x,y,'s','color','white');
 
+lines = houghlines(BW,T,R,P);
+
+figure, imshow(BW), hold on
+max_len = 0;
+for k = 1:length(lines)
+   xy = [lines(k).point1; lines(k).point2];
+   plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
+
+   % Plot beginnings and ends of lines
+   plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
+   plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
+
+   % Determine the endpoints of the longest line segment
+   len = norm(lines(k).point1 - lines(k).point2);
+   if ( len > max_len)
+      max_len = len;
+      xy_long = xy;
+   end
+end
+
+plot(xy_long(:,1),xy_long(:,2),'LineWidth',2,'Color','cyan');
+
+angle = radtodeg(tan((xy_long(1,2)-xy_long(2,2)) / (xy_long(1,1)-xy_long(2,1))));
+rotI = imrotate(I,angle,'crop');
 
 
+figure, imshow(rotI)
